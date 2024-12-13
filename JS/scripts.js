4960857268a5dc45091264404f8344850dd81654
MrByteBear <bjornbytes95@gmail.com>
2024-12-13 01:17:45 +0100

    Fixed CSS files. Implemented mobile Header + Navbar.

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,28 +1,37 @@
 // ***********  Variables **************
 // Containers (Burger Menu & Hide Burger Menu)
-const burger = document.querySelector(".burger");
+const burgerMenu = document.querySelector(".burgerMenu");
 const headerNav = document.querySelector(".headerNav");
-const menu = document.querySelector(".menu");
+const headerNavLinks = document.querySelector(".headerNavLinks");
+const scrollThreshold = 100;
 const toggleContainer = document.querySelector(".toggleable")
 
 // Toggle burger and headerNav on click
-burger.addEventListener("click", burgerClick); 
+burgerMenu.addEventListener("click", burgerClick); 
 function burgerClick() { 
-  burger.classList.toggle("active");
+  burgerMenu.classList.toggle("active");
   headerNav.classList.toggle("active");
 }
 
 // Close burger and headerNav when a menu item is clicked
-menu.addEventListener("click", menuClick);
-function menuClick() { 
-  burger.classList.remove("active");
+headerNavLinks.addEventListener("click", navLinksClick);
+function navLinksClick() { 
+  burgerMenu.classList.remove("active");
   headerNav.classList.remove("active");
 }
 
 // Reset the burger and headerNav state when resizing to desktop view
 window.addEventListener("resize", () => {
   if (window.innerWidth > 800) {
-    burger.classList.remove("active");
+    burgerMenu.classList.remove("active");
     headerNav.classList.remove("active");
+  }
+});
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > scrollThreshold) {
+    headerNav.classList.add("visible");
+  } else {
+    headerNav.classList.remove("visible");
   }
 });
